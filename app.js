3ec8b62437ff584ec1d2a602b8cfaae65d9b12e1
Riccardo P <riccardop@outlook.com>
2023-03-27 10:19:37 +1100

    Added code to write JSON file with the embedding data

--- app.js
+++ app.js
@@ -4,8 +4,9 @@ const { Configuration, OpenAIApi } = require('openai');
 const fs = require('fs');
 const { parse, stringify } = require('csv');
 
-const INPUT_DATA_FILE = __dirname + '/data/input/data.csv';
-const OUTPUT_DATA_FILE = __dirname + '/data/output/data.csv';
+const INPUT_DATA_CSV_FILE = __dirname + '/data/input/data.csv';
+const OUTPUT_DATA_CSV_FILE = __dirname + '/data/output/data.csv';
+const OUTPUT_DATA_JSON_FILE = __dirname + '/data/output/data.json';
 const EMBEDDING_MODEL = 'text-embedding-ada-002';
 
 const configuration = new Configuration({
@@ -17,10 +18,11 @@ const openai = new OpenAIApi(configuration);
 const embeddingsResults = []; //Array to hold results of text + embedding for each .csv row
 const embeddingsRequests = []; //Array to hold Promises for openai embeddings requests
 
-fs.createReadStream(INPUT_DATA_FILE)
+//Start reading the source .csv file
+fs.createReadStream(INPUT_DATA_CSV_FILE)
     .pipe(parse({delimiter: ',', from_line: 2}))
     .on('data', (row) => {
-        console.log('Reading one row.');
+        //console.log('Reading one row.');
         embeddingsRequests.push(getEmbeddings(row[0])); //For each .csv row we push a new embedding request (Promise) into the array. Later we'll load all requests with Promise.all()
     })
     .on('error', (err) => {
@@ -32,10 +34,13 @@ fs.createReadStream(INPUT_DATA_FILE)
     .on('close', () => {
         console.log('File closed.');
 
+        //Start all embedding requests to openai API
         Promise.all(embeddingsRequests)
             .then(() => {
                 console.log('Fulfilled all embeddings requests');
-                writeOuputCSV();
+                //Create a new .csv and .json file with the embedding data
+                writeOutputCSV();
+                writeOutputJSON();
             })
             .catch((err) => {
                 console.log('Error while implementing embedding request.');
@@ -52,20 +57,29 @@ const getEmbeddings = async (inputText) => {
     const newEntry = {
         text: inputText,
         embedding: response.data.data[0].embedding
-    }
+    };
 
     embeddingsResults.push(newEntry);
 }
 
-const writeOuputCSV = () => {
+const writeOutputCSV = () => {
     console.log('Writing data to new csv...');
     stringify(embeddingsResults, {header: true}, (err, output) => {
         try {
-            fs.writeFileSync(OUTPUT_DATA_FILE, output);
+            fs.writeFileSync(OUTPUT_DATA_CSV_FILE, output);
         } catch (error) {
             console.log(error);
         }
         
     });
-    console.log('Data writing completed.');
+    console.log('Data writing to CSV completed.');
+}
+
+const writeOutputJSON = () => {
+    console.log('Writing data to new JSON...');
+    const stringifiedJSON = JSON.stringify(embeddingsResults);
+
+    fs.writeFile(OUTPUT_DATA_JSON_FILE, stringifiedJSON, 'utf-8', () => {
+        console.log('Data writing to JSON completed.');
+    });
 }

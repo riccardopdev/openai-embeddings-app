e259c6d32fac30ace717696b6dcec5a43a1ca0f8
Riccardo P <riccardop@outlook.com>
2023-04-03 18:37:40 +1000

    Updated the code to create batches of requests to the OpenAI API based on the limit of number of requests per minute

--- app.js
+++ app.js
@@ -1,15 +1,29 @@
 // Make sure you have your OpenAI API Key value for OPENAI_API_KEY environment variable in an .env file. Otherwise replace below line of process.env.OPENAI_API_KEY with your OpenAI API Key value
 
+/* -------------- APP STEPS --------------------
+1) Read input data.csv: startReadingInputCSV()
+2) Create batches of API requests: createAPIBatchRequest()
+3) Submit the batches to OpenAI Embedding API: submitAPIBatchesRequest()
+4) Write output data to output/data.csv: writeOutputCSV()
+5) Write output data to output/data.json: writeOutputJSON
+*/
+
 // IMPORT PACKAGES
 require('dotenv').config();
 const { Configuration, OpenAIApi } = require('openai');
 const fs = require('fs');
+const path = require('path');
 const { parse, stringify } = require('csv');
 
-const INPUT_DATA_CSV_FILE = __dirname + '/data/input/data.csv';
-const OUTPUT_DATA_CSV_FILE = __dirname + '/data/output/data.csv';
-const OUTPUT_DATA_JSON_FILE = __dirname + '/data/output/data.json';
 const EMBEDDING_MODEL = 'text-embedding-ada-002';
+const INPUT_DATA_CSV_FILE = path.join(__dirname, '/data/input/data.csv');
+const OUTPUT_DATA_CSV_FILE = path.join(__dirname, '/data/output/data.csv');
+const OUTPUT_DATA_JSON_FILE = path.join(__dirname, '/data/output/data.json');
+
+// ----------- NOTE: OpenAI API request limit is 60 / min ----------- 
+// ########### Adjust below two constant based on OpenAI API request limits if these change  ########### 
+const NUM_OF_API_REQUEST_LIMIT = 60; //The number of API request to be submitted in one batch
+const TIME_LIMIT_FOR_API_REQUEST = 61000; //The time interval limit between batches API requests
 
 const configuration = new Configuration({
     apiKey: process.env.OPENAI_API_KEY
@@ -17,39 +31,82 @@ const configuration = new Configuration({
 
 const openai = new OpenAIApi(configuration);
 
-const embeddingsResults = []; //Array to hold results of text + embedding for each .csv row
+const rowContentFromCSV = []; //Array to hold the content from each row of the input/data.csv file
+const APIRequestBatches = []; //Array to hold the batches of API requests to be submitted to OpenAI API
 const embeddingsRequests = []; //Array to hold Promises for openai embeddings requests
+const embeddingsResults = []; //Array to hold results of text + embedding for each .csv row
 
-//Start reading the source .csv file
-fs.createReadStream(INPUT_DATA_CSV_FILE)
-    .pipe(parse({delimiter: ',', from_line: 2}))
-    .on('data', (row) => {
-        //console.log('Reading one row.');
-        embeddingsRequests.push(getEmbeddings(row[0])); //For each .csv row we push a new embedding request (Promise) into the array. Later we'll load all requests with Promise.all()
-    })
-    .on('error', (err) => {
-        console.log(err);
-    })
-    .on('end', () => {
-        console.log('Finished reading file.');
-    })
-    .on('close', () => {
-        console.log('File closed.');
+const startReadingInputCSV = () => {
+    //Start reading the source .csv file
+    fs.createReadStream(INPUT_DATA_CSV_FILE)
+        .pipe(parse({delimiter: ',', from_line: 2})) //We skip the first line as this is reserved for the "title"
+        .on('data', (row) => {
+            rowContentFromCSV.push(row[0]); //For each row we only retrieve content from the first column [0] and store its value in an array
+        })
+        .on('error', (err) => {
+            console.log('Error while reading source .csv file.');
+            console.log(err);
+        })
+        .on('end', () => {
+            console.log('Finished reading .csv file.');
+        })
+        .on('close', () => {
+            console.log('File closed.\n');
+            createAPIBatchRequest(); //Create batches of request based on the number limit of allowed API requests
+        });
+}
 
-        //Start all embedding requests to openai API
-        Promise.all(embeddingsRequests)
-            .then(() => {
-                console.log('Fulfilled all embeddings requests');
-                //Create a new .csv and .json file with the embedding data
-                writeOutputCSV();
-                writeOutputJSON();
-            })
-            .catch((err) => {
-                console.log('Error while implementing embedding request.');
-                console.log(err);
-            });
-    });
+//This function will split the content read from the .csv file in batches based on the number of OpenAI API request limit
+const createAPIBatchRequest = () => {
+    for (let i = 0; i < rowContentFromCSV.length; i++) {
+        if (i % NUM_OF_API_REQUEST_LIMIT === 0) {
+            //Create a new array within APIRequestBatches array the first time and every time we reach the API num limit
+            APIRequestBatches[APIRequestBatches.length] = new Array();
+        }
 
+        //Add the content from one row to the last batch
+        APIRequestBatches[APIRequestBatches.length - 1].push(rowContentFromCSV[i]);
+    }
+
+    //Once we have divided the content into batches we can start submitting each batch to the OpenAI API
+    submitAPIBatchesRequest();
+}
+
+const submitAPIBatchesRequest = () => {
+    console.log(`There are a total of ${APIRequestBatches.length} batch(es).`);
+    console.log(`This will take at least ${(APIRequestBatches.length * TIME_LIMIT_FOR_API_REQUEST)/60000} minutes.\n`);
+
+    //Loop through each batch
+    for (let i = 0; i < APIRequestBatches.length; i++) {
+        //Set a time interval between batches
+        setTimeout(() => {
+            console.log(`Submitting batch #${i+1} to OpenAI Embedding API.`);
+            //Loop through the items of each batch and 
+            for (let j = 0; j < APIRequestBatches[i].length; j++) {
+                embeddingsRequests.push(getEmbeddings(APIRequestBatches[i][j])); //For each item push a new embedding request (Promise) into the embeddingsRequests array. Later we'll check when all promises have been fulfilled with Promise.all()
+            }
+
+            //Check if we are at the last batch
+            if(i === APIRequestBatches.length - 1) {
+                //If we are at the last batch check when all Promises have been fulfilled
+                Promise.all(embeddingsRequests)
+                    .then(() => {
+                        console.log('Fulfilled all embeddings requests.\n');
+                        //Create a new .csv and .json file with the embedding data
+                        writeOutputCSV();
+                        writeOutputJSON();
+                    })
+                    .catch((err) => {
+                        console.log('Error while implementing embedding request.');
+                        console.log(err);
+                    });
+            }
+        }, i * TIME_LIMIT_FOR_API_REQUEST);
+        
+    }
+}
+
+//This function creates one single request to the OpenAI Embedding API
 const getEmbeddings = async (inputText) => {
     const response = await openai.createEmbedding({
         input: inputText,
@@ -61,6 +118,7 @@ const getEmbeddings = async (inputText) => {
         embedding: response.data.data[0].embedding
     };
 
+    //Store the embedding resuslts and text, which we will later use to write the data into a .csv and .json file
     embeddingsResults.push(newEntry);
 }
 
@@ -74,7 +132,7 @@ const writeOutputCSV = () => {
         }
         
     });
-    console.log('Data writing to CSV completed.');
+    console.log('Data writing to CSV completed.\n');
 }
 
 const writeOutputJSON = () => {
@@ -82,6 +140,8 @@ const writeOutputJSON = () => {
     const stringifiedJSON = JSON.stringify(embeddingsResults);
 
     fs.writeFile(OUTPUT_DATA_JSON_FILE, stringifiedJSON, 'utf-8', () => {
-        console.log('Data writing to JSON completed.');
+        console.log('Data writing to JSON completed.\n');
     });
 }
+
+startReadingInputCSV();
